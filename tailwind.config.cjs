ec4c614a635a811724b4ec763f3c77df3566927c
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2023-03-19 10:54:21 +0100

    første

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
-  theme: {
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    mytheme: {
+      "lilla": "#5856D6",
+              
+      "rosa": "#cca9f9",
+              
+      "lysRød": "#f26584",
+              
+      "hvid": "#FFFFF",
+              
+      "base-100": "#5ABE8C",
+              
+      "info": "#9BCDED",
+              
+      "success": "#74FF91",
+              
+      "warning": "#FACD47",
+              
+      "error": "#306EF9",
+    },
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require('daisyui')],
 };
