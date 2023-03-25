af31809c1af71f3ff3179b9f19db2d7a5869b265
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2023-03-26 00:48:08 +0100

    scroll op

--- src/main.js
+++ src/main.js
@@ -12,3 +12,21 @@ Alpine.start()
 // 		}
 //     document.getElementById('counterElement').style.setProperty('--value', counter)
 // }, 1000)
+
+
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('topBtn', () => ({
+        scrolltoTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    }));
+});
+
+const topBtn = document.getElementById("topButton");
+window.onscroll = () => {
+    (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ?
+    topBtn.classList.remove("hidden"): topBtn.classList.add("hidden");
+
+}
