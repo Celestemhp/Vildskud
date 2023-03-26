30de0383a750d68634ceecf51c4d126d3b38b30f
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2023-03-26 01:50:46 +0100

    alt fungerer

--- src/main.js
+++ src/main.js
@@ -15,18 +15,3 @@ Alpine.start()
 
 
 
-document.addEventListener('alpine:init', () => {
-    Alpine.data('topBtn', () => ({
-        scrolltoTop() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
-    }));
-});
-
-const topBtn = document.getElementById("topButton");
-window.onscroll = () => {
-    (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ?
-    topBtn.classList.remove("hidden"): topBtn.classList.add("hidden");
-
-}
